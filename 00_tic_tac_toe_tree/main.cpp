24885cc9226006849dc455cc18d2eca809d3ac35
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-04-05 22:56:57 -0300

    fix(main): change entire program logic
    change Class Node
    create Class Game
    create game logic with only game over for full board

--- 00_tic_tac_toe_tree/main.cpp
+++ 00_tic_tac_toe_tree/main.cpp
@@ -4,83 +4,115 @@
 
 class Node {
  public:
-  Node * next;
-  Node(Node * next);
-  void placeX(Node * root, int position);
-  void placeO(Node * root, int position);
-  void printBoard(int position);
-  char board[9];
+  Node();
+  char node_board[9];
+  Node ** children;
+  int child_count;
 };
 
-Node::Node(Node * next) {
-  this-> next = next;
+class Game {
+ public:
+  Game();
+  void placeX(int position);
+  void placeO(int position);
+  void printBoard();
+  int getRemainingMoves();
+ private:
+  char board[9];
+  int remaining_moves;
+};
+
+Node::Node() {
   for (int i = 0; i < 9; i++){
-    board[i] = ' ';
+    node_board[i] = ' ';
   }
 }
 
-void Node::placeX(Node * node, int position){
-  while (node->next != NULL){
-    node = node->next;
-  }
-
-  node->board[position] = 'X';
-
-}
-
-void Node::placeO(Node * node, int position){
-  while (node->next != NULL){
-    node = node->next;
-  }
-
-  node->board[position] = 'O';
-}
-
-void Node::printBoard(int position) {
+Game::Game() {
   for (int i = 0; i < 9; i++){
-    if (i == position){
-      std::cout << "_, ";
+    board[i] = i + 48;
+  }
+  remaining_moves = 9;
+}
+
+void Game::placeX(int position) {
+  board[position] = 'X';
+  remaining_moves--;
+}
+
+void Game::placeO(int position) {
+  board[position] = 'O';
+  remaining_moves--;
+}
+
+void Game::printBoard() {
+  for (int i = 0; i < 3; i++){
+    if (i == 2){
+      std::cout << board[i];
     }else{
-      std::cout << board[i] << ", ";
-    }      
-  }
+      std::cout << board[i] << " | ";
+    }
+  }      
+  std::cout << std::endl;
+  std::cout << "-   -   -";
+  std::cout << std::endl;
+  for (int i = 3; i < 6; i++){
+    if (i == 5){
+      std::cout << board[i];
+    }else{
+      std::cout << board[i] << " | ";
+    }
+  }      
+  std::cout << std::endl;
+  std::cout << "-   -   -";
+  std::cout << std::endl;
+  for (int i = 6; i < 9; i++){
+    if (i == 8){
+      std::cout << board[i];
+    }else{
+      std::cout << board[i] << " | ";
+    }
+  }      
   std::cout << std::endl;
 }
 
-void displayPossibilities(Node * node){
-  while (node->next != NULL){
-    node = node->next;
-  }
+int Game::getRemainingMoves() {
+  return remaining_moves;
+}
 
-  for (int i = 0; i < 9; i++){
-    if (node->board[i] == ' '){
-      std::cout << "Position " << std::to_string(i) <<": ";
-      node->printBoard(i);
-    }
-  }
+Node * createTree(Game * game){
+  Node * node = new Node();
+  node->child_count = game->getRemainingMoves();
+  return node;
 }
 
 int main() {
-  Node * root;
-  root = new Node(NULL);
   bool x_turn = true;
   int position = 0;
+  Game * game = new Game();
+  Node * root;
+  root = createTree(game);
   
-  while(true){
+  while(game->getRemainingMoves() != 0){
     if (x_turn){
       std::cout << "Select a position to place the X: "<< std::endl;
-      displayPossibilities(root);
+      game->printBoard();
       std::cin >> position;
-      root->placeX(root,position);
+      game->placeX(position);
       x_turn = false;
-
     }else{
       std::cout << "Select a position to place the O: "<< std::endl;
-      displayPossibilities(root);
+      game->printBoard();
       std::cin >> position;
-      root->placeO(root,position);
+      game->placeO(position);
       x_turn = true;
     }
+    std::cout << "\033[2J\033[1;1H";
   }
+
+  std::cout << "Game Over" << std::endl;
+  game->printBoard();
+  printf("%d /n",game->getRemainingMoves());
+
   return 0;
 }
