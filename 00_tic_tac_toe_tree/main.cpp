169c7a58fb5f58c3d81f2b4587579eccea627145
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-04-06 18:36:03 -0300

    feat(main): add depth to the tree, tree still not correct

--- 00_tic_tac_toe_tree/main.cpp
+++ 00_tic_tac_toe_tree/main.cpp
@@ -42,7 +42,7 @@ void Node::placeO(int position) {
 
 Game::Game() {
   for (int i = 0; i < 9; i++){
-    board[i] = i + 48;
+    board[i] = ' ';
   }
   remaining_moves = 9;
 }
@@ -60,9 +60,17 @@ void Game::placeO(int position) {
 void Game::printBoard() {
   for (int i = 0; i < 3; i++){
     if (i == 2){
-      std::cout << board[i];
+      if (board[i] == ' '){
+        std::cout << i;
+      }else{
+        std::cout << board[i];
+      }
     }else{
-      std::cout << board[i] << " | ";
+      if (board[i] == ' '){
+        std::cout << i << " | ";
+      }else{
+        std::cout << board[i] << " | ";
+      }
     }
   }      
   std::cout << std::endl;
@@ -70,9 +78,17 @@ void Game::printBoard() {
   std::cout << std::endl;
   for (int i = 3; i < 6; i++){
     if (i == 5){
-      std::cout << board[i];
+      if (board[i] == ' '){
+        std::cout << i;
+      }else{
+        std::cout << board[i];
+      }
     }else{
-      std::cout << board[i] << " | ";
+      if (board[i] == ' '){
+        std::cout << i << " | ";
+      }else{
+        std::cout << board[i] << " | ";
+      }
     }
   }      
   std::cout << std::endl;
@@ -80,9 +96,17 @@ void Game::printBoard() {
   std::cout << std::endl;
   for (int i = 6; i < 9; i++){
     if (i == 8){
-      std::cout << board[i];
+      if (board[i] == ' '){
+        std::cout << i;
+      }else{
+        std::cout << board[i];
+      }
     }else{
-      std::cout << board[i] << " | ";
+      if (board[i] == ' '){
+        std::cout << i << " | ";
+      }else{
+        std::cout << board[i] << " | ";
+      }
     }
   }      
   std::cout << std::endl;
@@ -94,7 +118,6 @@ int Game::getRemainingMoves() {
 
 Node * createTree(int remaining_moves, char board[9], int depth){
   Node * node = new Node();
-  bool x_turn = true;
 
   node->child_count = remaining_moves;
 
@@ -102,21 +125,18 @@ Node * createTree(int remaining_moves, char board[9], int depth){
     node->children = new Node * [node->child_count];
 
     for (int i = 0; i != node->child_count; ++i){
-      if (x_turn){
-        node->placeX(i);
-        node->children[i] = createTree(depth - 1,node->node_board, depth - 1);
-        x_turn = false;
-        total_children++;
-      }else{
-        node->placeO(i);
-        node->children[i] = createTree(depth - 1,node->node_board, depth - 1);
-        x_turn = true;
-        total_children++;
-      }        
+      node->placeX(i);
+      node->children[i] = createTree(depth - 1,node->node_board, depth - 1);
+      total_children++;
     }
   }else{
     node->children = NULL;
   }
+
+  // for (int i = 0; i < 9; ++i) {
+  //   std::cout << node->node_board[i] << "  ";
+  // }
+  // std::cout << std::endl;
 
   return node;
 }
@@ -133,12 +153,14 @@ void deleteTree(Node * node)
 int main() {
   bool x_turn = true;
   int position = 0;
+  int depth = 9;
   Game * game = new Game();
   
   while(game->getRemainingMoves() != 0){
     Node * root;
-    root = createTree(game->getRemainingMoves(), game->board, 9);
-    printf("%d",total_children);
+    root = createTree(game->getRemainingMoves(), game->board, depth);
+
+    printf("Possible boards: %d", total_children);
     std::cout << std::endl;
 
     if (x_turn){
@@ -157,11 +179,12 @@ int main() {
     std::cout << "\033[2J\033[1;1H";
 
     deleteTree(root);
+    depth--;
   }
 
-  std::cout << "Game Over" << std::endl;
+  std::cout << "Remaining Moves: " << game->getRemainingMoves();
+  std::cout << " - Game Over" << std::endl;
   game->printBoard();
-  printf("%d /n",game->getRemainingMoves());
 
   return 0;
 }
